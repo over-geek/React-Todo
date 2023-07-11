cc6034c29efd31666a854dd807e10c7de805926c
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-11 10:50:07 +0000

    Refactor TodosLogic.jsx to use localStorage instead of staic code

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 
 const TodosLogic = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: true,
-    },
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: false,
-    },
-    {
-      id: uuidv4(),
-      title: 'Setup development environment',
-      completed: false,
-    },
-  ])
+  const getInitialTodos = () => {
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || [];
+  }
+  const [todos, setTodos] = useState(getInitialTodos());
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp)
+  }, [todos]);
   const handleChange = (id) => {
     setTodos((prevState) =>
       prevState.map((todo) => {
